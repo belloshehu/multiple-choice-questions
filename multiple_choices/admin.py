1f820c090bfcced2a4d3920a50f50d075c6d2ed4
ardesp <belloshehu1@gmail.com>
2020-08-30 11:24:41 +0100

    Register Models in Admin

--- multiple_choices/admin.py
+++ multiple_choices/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Choice, MultipleChoiceQuestion, AssessmentTaker
+
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(MultipleChoiceQuestion)
+admin.site.register(AssessmentTaker)
+
+admin.site.site_header = 'Multiple Choices Question Admin.'

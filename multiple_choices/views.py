09e0b019a0f354c8c0bade82aff1ebff95c77e1b
ardesp <belloshehu1@gmail.com>
2020-08-30 14:23:34 +0100

    Add home view

--- multiple_choices/views.py
+++ multiple_choices/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import AssessmentForm
+from .models import AssessmentTaker
 # Create your views here.
+
+
+def home(request):
+    ''' View for assessment taking. '''
+    return render(request, 'multiple_choices/home.html')
+
+
+def register(request):
+    pass
+
+def assessment(request):
+    pass

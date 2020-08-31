cd939970cb609d17b128a49acc71b012939c5885
ardesp <belloshehu1@gmail.com>
2020-08-31 21:04:12 +0100

    Add items in ALLOWED_HOSTS

--- assessment/settings.py
+++ assessment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['multi-choices.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
